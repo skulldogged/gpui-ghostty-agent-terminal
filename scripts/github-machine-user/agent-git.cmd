cc211d57c09d7340129069b26bcb5b1d75095fe0
skullbotted <319073449+skullbotted@users.noreply.github.com>
2026-08-22 20:48:39 -0400

    Prefer Git Bash in Windows command shims

--- scripts/github-machine-user/agent-git.cmd
+++ scripts/github-machine-user/agent-git.cmd
@@ -1,8 +1,9 @@
 @echo off
 setlocal
 set "AGENT_GIT_BASH="
-for %%I in (bash.exe) do set "AGENT_GIT_BASH=%%~$PATH:I"
-if not defined AGENT_GIT_BASH if exist "%ProgramFiles%\Git\bin\bash.exe" set "AGENT_GIT_BASH=%ProgramFiles%\Git\bin\bash.exe"
+if exist "%ProgramFiles%\Git\bin\bash.exe" set "AGENT_GIT_BASH=%ProgramFiles%\Git\bin\bash.exe"
+if not defined AGENT_GIT_BASH if exist "%ProgramFiles(x86)%\Git\bin\bash.exe" set "AGENT_GIT_BASH=%ProgramFiles(x86)%\Git\bin\bash.exe"
+if not defined AGENT_GIT_BASH for %%I in (bash.exe) do set "AGENT_GIT_BASH=%%~$PATH:I"
 if not defined AGENT_GIT_BASH (
   echo agent-git: Git for Windows bash.exe was not found 1>&2
   exit /b 1
